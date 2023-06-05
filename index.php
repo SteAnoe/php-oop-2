fdaa716b0d2fa4fd777a21cd1aa414a42f77425e
SteAnoe <ste.anoe@gmail.com>
2023-06-05 14:16:33 +0200

    finito

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 class Prodotto {
+    use rangeEtà;
+
     public $id;
     public $nome;
     public $prezzo;
@@ -8,21 +10,27 @@ class Prodotto {
     public $categoria;
     public $tipo;
     
-    public function __construct($id, $nome, $prezzo, $immagine, $categoria, $tipo) {
+    
+    public function __construct($id, $nome, $prezzo, $immagine, $categoria, $tipo, $eta) {
         $this->id = $id;
         $this->nome = $nome;
         $this->prezzo = $prezzo;
         $this->immagine = $immagine;
         $this->categoria = $categoria;
         $this->tipo = $tipo;
+        $this->setAge($eta);
+        if (empty($nome)) {
+            throw new Exception("Il prodotto non ha tutte le informazioni necessarie.");
+        }
     }
+    
 }
 
 class Categoria {
     public $id;
     public $nome;
     public $icona;
-    
+
     public function __construct($id, $nome, $icona) {
         $this->id = $id;
         $this->nome = $nome;
@@ -30,7 +38,16 @@ class Categoria {
     }
 }
 
+trait rangeEtà {
+    public $eta;
 
+    public function setAge($eta){
+        $this->eta = $eta;
+    }
+    public function getAge() {
+        return $this->eta;
+    }
+}
 
 // Creazione di una categoria
 $categoriaCani = new Categoria(1, "Cane", "fa-solid fa-dog");
@@ -38,14 +55,17 @@ $categoriaGatto = new Categoria(2, "Gatto", "fa-solid fa-cat");
 
 // Creazione di un prodotto
 $prodotti = [
-    new Prodotto(1, "Cibo per cani", 10.99, "https://www.naturepetshop.it/wp-content/uploads/cibo-secco-per-cani-300x215.jpg", $categoriaCani, "Cibo"),
-    new Prodotto(2, "Cibo per gatti", 15.99, "https://www.my-personaltrainer.it/2021/04/13/cibo-per-gatti_900x760.jpeg", $categoriaGatto, "Cibo"),
-    new Prodotto(3, "Palla per cani", 20.99, "https://t1.ea.ltmcdn.com/it/posts/3/8/6/insegnare_al_cane_a_riportare_la_palla_passo_per_passo_683_600.jpg", $categoriaCani, "Palla"),
-    new Prodotto(4, "Cuccia per gatti", 25.99, "https://croci.net/wp-content/uploads/2020/09/8023222228832.PT03.jpg", $categoriaGatto, "Cuccia"),
+    new Prodotto(1, "Cibo per cani", 10.99, "https://www.naturepetshop.it/wp-content/uploads/cibo-secco-per-cani-300x215.jpg", $categoriaCani, "Cibo", "Adulti"),
+    new Prodotto(2, "Cibo per gatti", 15.99, "https://www.my-personaltrainer.it/2021/04/13/cibo-per-gatti_900x760.jpeg", $categoriaGatto, "Cibo", "Cuccioli"),
+    new Prodotto(3, "Palla per cani", 20.99, "https://t1.ea.ltmcdn.com/it/posts/3/8/6/insegnare_al_cane_a_riportare_la_palla_passo_per_passo_683_600.jpg", $categoriaCani, "Palla", "Cuccioli"),
+    new Prodotto(4, "Cuccia per gatti", 25.99, "https://croci.net/wp-content/uploads/2020/09/8023222228832.PT03.jpg", $categoriaGatto, "Cuccia", "Adulti"),
 ];
 
-
-
+try {
+    new Prodotto(1, "", 10.99, "https://www.naturepetshop.it/wp-content/uploads/cibo-secco-per-cani-300x215.jpg", $categoriaCani, "Cibo", "Adulti");
+} catch (Exception $e) {
+    echo "Errore: " . $e->getMessage();
+}
 
 ?>
 
@@ -54,7 +74,7 @@ $prodotti = [
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>E-comm Animali</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
@@ -107,6 +127,9 @@ $prodotti = [
                     <div class="modal-body">
                         <div>
                             Prezzo: €<?php echo $elem->prezzo ?>
+                        </div>
+                        <div>
+                            Prodotto <?php echo $elem->tipo ?> per <?php echo $elem->getAge() ?>
                         </div>
                     </div>
                     <div class="modal-footer">
